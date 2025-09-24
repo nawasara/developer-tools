845125c8dc2b3799c53108fefa16c1ccc550b07c
Pringgo Juni S <odyinggo@gmail.com>
2025-09-24 10:09:31 +0700

    add x-cloak

--- resources/views/livewire/components/developer-tools.blade.php
+++ resources/views/livewire/components/developer-tools.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: @entangle('isOpen') }">
+<div x-data="{ open: @entangle('isOpen') }" x-cloak>
     <!-- Floating Button -->
     <button @click="open = true"
         class="fixed bottom-6 right-6 w-14 h-14 bg-blue-600 text-white rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 flex items-center justify-center z-50 transition-all duration-300">
